9c40cf6c840763bfdf90d9e5e383febfa6a2fca7
Rafael Dantas <rafaeldantas@Rafaels-MacBook-Pro.local>
2024-04-10 11:37:42 +0100

    First JS function and CSS

---
+++ showTip.js
@@ -0,0 +1,12 @@
+
+function changePhrase(){
+    console.log("oi");
+
+    const element = document.getElementById("phrase")
+    console.log(element)
+
+    element.textContent = "frase melhor"
+    
+
+    
+}
