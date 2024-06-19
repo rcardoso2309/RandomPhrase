f621f34f8e8aa7e1a85fe1ba7fe7be998743711a
Rafael Dantas <dantas85.ie@gmail.com>
2024-06-19 11:57:16 +0100

    updated function changePhrase

--- showTip.js
+++ showTip.js
@@ -1,12 +1,19 @@
+const phrases = [
+    "Being Irish, he had an abiding sense of tragedy, which sustained him through temporary periods of joy. - W.B. Yeats",
+    "To live is the rarest thing in the world. Most people exist, that is all. - Oscar Wilde",
+    "May your troubles be less and your blessings be more, and nothing but happiness come through your door.",
+    "Imagination is more important than knowledge. - Albert Einsten",
+    "It's a lovely day for a pint of Guinness"
+]
+
+
+let counter = 0
 
 function changePhrase(){
-    console.log("oi");
-
     const element = document.getElementById("phrase")
-    console.log(element)
-
-    element.textContent = "frase melhor"
-    
-
-    
+    element.textContent = phrases[counter]
+    counter = counter + 1
+    if(counter === phrases.length){
+        counter = 0
+    }
 }
